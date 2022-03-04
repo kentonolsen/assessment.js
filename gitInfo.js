88128e40aab74d1627bae7d928189f25fd476c29
Kenton Olsen <kentonolsen@gmail.com>
2022-03-04 08:47:26 -0700

    Intial Commit

---
+++ gitInfo.js
@@ -0,0 +1,76 @@
+/*
+    For this section of the assessment you will be putting together a cheat sheet for common git commands.  
+    You'll provide the command as well as what it does. 
+*/
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Create a variable called 'gitDefinition'.
+    It should be a string containing your best definition of what Git is.
+*/
+
+//CODE HERE
+var gitDefinition = `A terminal interface to navigate data drives without a GUI`
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable called 'gitHubDefinition'.  
+    It should be a string containing your best definition of what GitHub is.
+*/
+
+//CODE HERE
+var gitHubDefinition = `A website for storing snapshots of code off of the primary machine`
+//////////////////PROBLEM 3////////////////////
+/*
+    Create a variable called 'gitInitDefinition'.  
+    It should be a string containing your best definition of what 'git init' does.
+*/
+
+//CODE HERE
+var gitInitDefinition = `Git Init initializes Git in the folder currently opened`
+//////////////////PROBLEM 4////////////////////
+/*
+    Create a variable called 'gitCloneDefinition'.  
+    It should be a string containing your best definition of what 'git clone' does.
+*/
+
+//CODE HERE
+var gitCloneDefinition = `Git Clone pulls code from Github from a url you paste in after the command`
+//////////////////PROBLEM 5////////////////////
+/*
+    Create a variable called 'gitStatusDefinition'.  
+    It should be a string containing your best definition of what 'git status' does.
+*/
+
+//CODE HERE
+var gitStatusDefinition = `Git Status gives information on the current repository`
+//////////////////PROBLEM 6////////////////////
+/*
+    Create a variable called 'gitAddDefinition'.  
+    It should be a string containing your best definition of what 'git add' does.
+
+    Create another variable called 'gitAddCode'.  
+    It should be a string containing the code to add all files.
+*/
+
+//CODE HERE
+var gitAddDefinition = `Git Add adds files to be monitored by Git`
+//////////////////PROBLEM 7////////////////////
+var gitAddCode = `git add ..`
+/*
+    Create a variable called 'gitCommitDefinition'.  
+    It should be a string containing your best definition of what 'git commit' does.
+
+    Create a variable called 'gitCommitCode'.  
+    It should be a string containing the code to commit using the message "initial commit".
+*/
+
+//CODE HERE
+var gitCommitDefinition = `Git Commit takes a snapshot of all files added to Git and saves them in a different format`
+var gitCommitCode = `git commit -m'initial commit'`
+//////////////////PROBLEM 8////////////////////
+/*
+    Create a variable called 'gitPushDefinition'.  
+    It should be a string containing your best definition of what 'git push' does.
+*/
+//CODE HERE
+var gitPushDefinition = `Git Push takes all commited files and pushes them to a repository on Github`
